ba30e2e6c324463f1c5a048c21a8998c77f69e9c
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-10-09 19:45:28 +0900

    素数検索実装

--- pages/prime-search.vue
+++ pages/prime-search.vue
@@ -2,9 +2,9 @@
     <v-col>
         <v-row>
             <v-col>
-                <v-text-field v-model="searchTxt" append-outer-icon="mdi-magnify" label="Search" single-line
-                    hide-details :rules="[value => /^[0-9TJQK]+$/.test(value) || 'Invalid sequence.']"
-                    @click:append-outer="search(searchTxt)">
+                <v-text-field v-model="strSearch" append-outer-icon="mdi-magnify" label="Search" single-line
+                    hide-details :rules="[validQkStringQ || 'Invalid sequence.']"
+                    @click:append-outer="search(strSearch)" @keydown.enter="search(strSearch)">
                 </v-text-field>
             </v-col>
         </v-row>
@@ -34,14 +34,14 @@
 
         <v-bottom-sheet v-model="cardSheet" dark>
             <v-sheet height="450px" color="grey lighten-1">
-                <v-text-field v-model="searchTxt" append-outer-icon="mdi-magnify" label="Search" single-line
-                    hide-details class="ma-5" @click:append-outer="search(searchTxt)">
+                <v-text-field v-model="strSearch" append-outer-icon="mdi-magnify" label="Search" single-line
+                    hide-details class="ma-5" @click:append-outer="search(strSearch)">
                 </v-text-field>
                 <v-row class="justify-center my-5">
                     <v-col cols="10" md="3">
                         <v-row v-for="y in [0, 1, 2]" :key="y">
                             <v-col v-for="x in [0, 1, 2, 3, 4]" :key="x" class="pa-1"
-                                @click="searchTxt += { 10: 'T', 11: 'J', 12: 'Q', 13: 'K', 14: 'X' }[x + y * 5] || x + y * 5">
+                                @click="strSearch += { 10: 'T', 11: 'J', 12: 'Q', 13: 'K', 14: 'X' }[x + y * 5] || x + y * 5">
                                 <Card :is-disable="x + y * 5 > 13" :number=" x + y * 5 === 14 ? Infinity : x + y * 5">
                                 </Card>
                             </v-col>
@@ -52,24 +52,24 @@
         </v-bottom-sheet>
         <v-bottom-sheet v-model="numberSheet">
             <v-sheet height="380px" color="grey lighten-1">
-                <v-text-field v-model="searchTxt" append-outer-icon="mdi-magnify" label="Search" single-line
-                    hide-details class="ma-5" @click:append-outer="search(searchTxt)">
+                <v-text-field v-model="strSearch" append-outer-icon="mdi-magnify" label="Search" single-line
+                    hide-details class="ma-5" @click:append-outer="search(strSearch)">
                 </v-text-field>
                 <v-row class="justify-center my-5">
                     <v-col cols="10" md="3">
                         <v-row v-for="y in [0, 1, 2]" :key="y" class="text-center">
-                            <v-col v-for="x in [0, 1, 2]" :key="x" class="pa-1" @click="searchTxt += x + y * 3 + 1">
+                            <v-col v-for="x in [0, 1, 2]" :key="x" class="pa-1" @click="strSearch += x + y * 3 + 1">
                                 <v-btn fab>{{ x + y * 3 + 1 }}</v-btn>
                             </v-col>
                         </v-row>
                         <v-row class="text-center">
-                            <v-col class="pa-1" @click="searchTxt += X">
+                            <v-col class="pa-1" @click="strSearch += X">
                                 <v-btn fab disabled>X</v-btn>
                             </v-col>
-                            <v-col class="pa-1" @click="searchTxt += '0'">
+                            <v-col class="pa-1" @click="strSearch += '0'">
                                 <v-btn fab>0</v-btn>
                             </v-col>
-                            <v-col class="pa-1" @click="searchTxt = searchTxt.slice(0, -1)">
+                            <v-col class="pa-1" @click="strSearch = strSearch.slice(0, -1)">
                                 <v-btn fab>
                                     <v-icon small>mdi-backspace</v-icon>
                                 </v-btn>
@@ -103,32 +103,42 @@ const primeQ = x => {
     return true
 }
 
-const parseQkText = txt => {
-    const txtRepl = txt.toUpperCase()
-        .replace("A", "1")
-        .replace("T", "10")
-        .replace("J", "11")
-        .replace("Q", "12")
-        .replace("K", "13")
-
-    const ret = BigInt(txtRepl)
-
-    return ret
-}
-
 export default {
     name: "PrimeSearch",
     data() {
         return {
             cardSheet: false,
             numberSheet: false,
-            searchTxt: "",
+            strSearch: "",
             result: null,
         };
     },
     methods: {
-        search(txt) {
-            const num = parseQkText(txt)
+        validQkStringQ(val) {
+            return /^[0-9ATJQKatjqk]+$/.test(val)
+        },
+        parseQkString(str) {
+            if (!this.validQkStringQ(str)) {
+                return NaN
+            }
+
+            const strRepl = str.toUpperCase()
+                .replace("A", "1")
+                .replace("T", "10")
+                .replace("J", "11")
+                .replace("Q", "12")
+                .replace("K", "13")
+
+            const ret = BigInt(strRepl)
+
+            return ret
+        },
+        search(str) {
+            const num = this.parseQkString(str)
+
+            if (Number.isNaN(num)) {
+                return
+            }
 
             const isPrime = primeQ(num)
             const isX2 = (num > 2n) && (num % 2n)
