7ef63c9d01f0071ebbfe3057e5b3967fcf5e4f4f
iwabuchi-yuki-butchi <butchiyu+com-github@gmail.com>
2022-10-09 13:57:16 +0900

    app修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,19 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar color="primary" dark fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
@@ -16,6 +29,13 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'ホーム',
+          to: '/',
+        },
       title: 'prime-qk.com',
     }
   },
